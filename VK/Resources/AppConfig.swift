ebfddbee3495c175ca746ac2361b2eff875c7794
evgeniy-bizhanov <evgeniy.bizhanov@me.com>
2019-02-04 18:46:49 +0700

    Added app config file

---
+++ VK/Resources/AppConfig.swift
@@ -0,0 +1,19 @@
+//
+//  AppConfig.swift
+//  VK
+//
+//  Created by Евгений Бижанов on 04/02/2019.
+//  Copyright © 2019 Евгений Бижанов. All rights reserved.
+//
+
+struct AppConfig {
+    
+    
+    // MARK: - Authentication
+    
+    struct Authentication {
+        static let id = "6349018"
+        static let key = "hg792B7zrq7AOY73MZrv"
+    }
+    
+}
