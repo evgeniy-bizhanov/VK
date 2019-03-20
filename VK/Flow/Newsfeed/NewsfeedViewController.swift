558eae9216055bd95f02f268b97891fb66abcac4
Evgeniy <e.bizhanov@magdv.com>
2019-03-20 14:36:30 +0700

    Added separator to sections

--- VK/Flow/Newsfeed/NewsfeedViewController.swift
+++ VK/Flow/Newsfeed/NewsfeedViewController.swift
@@ -52,6 +52,52 @@ extension NewsfeedViewController: NewsfeedOutput {
 extension NewsfeedViewController: UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 8
+        if section == 0 {
+            return 8
+        }
+        
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        if section == tableView.numberOfSections - 1 {
+            return 8
+        }
+        
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UITableViewHeaderFooterView()
+        
+        let line = UIView()
+        line.translatesAutoresizingMaskIntoConstraints = false
+        line.backgroundColor = UIColor(red: 225/255, green: 225/255, blue: 225/255, alpha: 1)
+        view.addSubview(line)
+        
+        line.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        line.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        line.topAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        return view
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let view = UITableViewHeaderFooterView()
+        
+        let line = UIView()
+        line.translatesAutoresizingMaskIntoConstraints = false
+        line.backgroundColor = UIColor(red: 225/255, green: 225/255, blue: 225/255, alpha: 1)
+        view.addSubview(line)
+        
+        line.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        line.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        line.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        
+        return view
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.separatorInset = UIEdgeInsets(top: 0, left: cell.frame.maxX, bottom: 0, right: 0)
     }
 }
